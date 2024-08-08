007a77cb35719af101c0c4fd0440f92d82cf2bed
KIRUTHIKASUBRAMANIANN <104428164+KIRUTHIKASUBRAMANIANN@users.noreply.github.com>
2024-08-08 19:18:31 +0530

    Add files via upload

--- Week_2_Exercises/spring/librarymanagement/src/main/java/com/library/service/BookService.java
+++ Week_2_Exercises/spring/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -1,7 +1,16 @@
 package com.library.service;
+import com.library.repository.BookRepository;
 
 public class BookService {
-    public void printService() {
-        System.out.println("BookService is called.");
+    private BookRepository bookRepository;
+
+    // Setter method for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Example method to demonstrate functionality
+    public void performService() {
+        bookRepository.displayRepositoryInfo();
     }
 }
